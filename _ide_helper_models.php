497879246bc8370b90d63ff17c3a7ffd7f3475fd
danalex <daniil.alex1@gmail.com>
2022-06-09 22:54:40 +0300

    created ide helper for showing helper methods

---
+++ _ide_helper_models.php
@@ -0,0 +1,69 @@
+<?php
+
+// @formatter:off
+
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models {
+    /**
+     * App\Models\Subscriber
+     *
+     * @property int $id
+     * @property string $email
+     * @method static \Illuminate\Database\Eloquent\Builder|Subscriber newModelQuery()
+     * @method static \Illuminate\Database\Eloquent\Builder|Subscriber newQuery()
+     * @method static \Illuminate\Database\Eloquent\Builder|Subscriber query()
+     * @method static \Illuminate\Database\Eloquent\Builder|Subscriber whereEmail($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|Subscriber whereId($value)
+     */
+    class Subscriber extends \Eloquent
+    {
+    }
+}
+
+namespace App\Models {
+
+    use Illuminate\Database\Eloquent\Collection;
+    use Illuminate\Notifications\DatabaseNotificationCollection;
+    use Illuminate\Support\Carbon;
+
+    /**
+     * App\Models\User
+     *
+     * @property int $id
+     * @property string $name
+     * @property string $email
+     * @property Carbon|null $email_verified_at
+     * @property string $password
+     * @property string|null $remember_token
+     * @property Carbon|null $created_at
+     * @property Carbon|null $updated_at
+     * @property-read DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+     * @property-read int|null $notifications_count
+     * @property-read Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+     * @property-read int|null $tokens_count
+     * @method static \Database\Factories\UserFactory factory(...$parameters)
+     * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+     * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+     * @method static \Illuminate\Database\Eloquent\Builder|User query()
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+     * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+     */
+    class User extends \Eloquent
+    {
+    }
+}
+
